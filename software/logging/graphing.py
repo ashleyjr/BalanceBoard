19b53df05ea8e532cf4301924a88581e0e4cd484
Ashley J. Robinson <ashley181291@gmail.com>
2023-12-15 22:24:39 +0000

    graphs

--- software/logging/graphing.py
+++ software/logging/graphing.py
@@ -1,9 +1,15 @@
+import matplotlib
+matplotlib.use('GTK3Agg')
 import matplotlib.pyplot as plt
-import numpy as np
 import argparse
 
 
 MPU_SAMPLE_PERIOD_S = 5e-3
+MPU_FS_SEL = 500
+MPU_AFS_SEL = 2
+MPU_FS_SEL_DIV = (2 ** 16) / (2*MPU_FS_SEL)
+MPU_AFS_SEL_DIV = (2 ** 16) / (2*MPU_AFS_SEL)
+MOTOR_DRV = 100
 
 class Analysis:
 
@@ -28,6 +34,7 @@ class Analysis:
         self.__gyro_x = []
         self.__gyro_y = []
         self.__gyro_z = []
+        self.__motor = []
         lines = log.split("\n")[:-1]
         for line in lines:
             self.__time.append(self.__procLineTime(line))
@@ -37,6 +44,7 @@ class Analysis:
             self.__gyro_x.append(self.__procLineGyroX(line))
             self.__gyro_y.append(self.__procLineGyroY(line))
             self.__gyro_z.append(self.__procLineGyroZ(line))
+            self.__motor.append(self.__procLineMotor(line))
 
     def __procLineTime(self, line):
         s = line.split(",")[0]
@@ -47,22 +55,25 @@ class Analysis:
         return ((self.laps * (2 ** 16)) + i) *  MPU_SAMPLE_PERIOD_S
 
     def __procLineAccelX(self, line):
-        return self.__procTwosComp(line.split(",")[1])
+        return self.__procTwosComp(line.split(",")[1]) / MPU_AFS_SEL_DIV
 
     def __procLineAccelY(self, line):
-        return self.__procTwosComp(line.split(",")[2])
+        return self.__procTwosComp(line.split(",")[2]) / MPU_AFS_SEL_DIV
 
     def __procLineAccelZ(self, line):
-        return self.__procTwosComp(line.split(",")[3])
+        return self.__procTwosComp(line.split(",")[3]) / MPU_AFS_SEL_DIV
 
     def __procLineGyroX(self, line):
-        return self.__procTwosComp(line.split(",")[4])
+        return self.__procTwosComp(line.split(",")[4]) / MPU_FS_SEL_DIV
 
     def __procLineGyroY(self, line):
-        return self.__procTwosComp(line.split(",")[5])
+        return self.__procTwosComp(line.split(",")[5]) / MPU_FS_SEL_DIV
 
     def __procLineGyroZ(self, line):
-        return self.__procTwosComp(line.split(",")[6])
+        return self.__procTwosComp(line.split(",")[6]) / MPU_FS_SEL_DIV
+
+    def __procLineMotor(self, line):
+        return self.__procTwosComp(line.split(",")[7])
 
     def __procTwosComp(self, s):
         i = int(s, 16)
@@ -85,47 +96,27 @@ class Analysis:
         return self.__accel_z
 
     def plotAll(self):
-        plt.plot(self.__time, self.__accel_x)
-        plt.plot(self.__time, self.__accel_y)
-        plt.plot(self.__time, self.__accel_z)
-        plt.plot(self.__time, self.__gyro_x)
-        plt.plot(self.__time, self.__gyro_y)
-        plt.plot(self.__time, self.__gyro_z)
-        plt.grid(True, which='major')
-        plt.grid(True, which='minor')
-        plt.legend()
-        plt.xlabel("Position")
-        plt.ylabel("Time (ns)")
+        fig, ax = plt.subplots(3,1)
+        ax[0].plot(self.__time, self.__accel_x, label="x")
+        ax[0].plot(self.__time, self.__accel_y, label="y")
+        ax[0].plot(self.__time, self.__accel_z, label="z")
+        ax[1].plot(self.__time, self.__gyro_x, label="x")
+        ax[1].plot(self.__time, self.__gyro_y, label="y")
+        ax[1].plot(self.__time, self.__gyro_z, label="z")
+        ax[2].plot(self.__time, self.__motor)
+        for i in range(3):
+            ax[i].grid(True, which='major')
+            ax[i].grid(True, which='minor')
+            ax[i].legend()
+            ax[i].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        ax[1].set_label("Time(s)")
+        ax[0].set_ylabel("Accel (g)")
+        ax[1].set_ylabel("Gyro (º/s)")
+        ax[2].set_ylabel("Motor Drive (+/-%)")
+        ax[0].set_ylim([-MPU_AFS_SEL,MPU_AFS_SEL])
+        ax[1].set_ylim([-MPU_FS_SEL,MPU_FS_SEL])
+        ax[2].set_ylim([-MOTOR_DRV, MOTOR_DRV])
         plt.savefig("graph.png", dpi=200)
-
-
-
-#    def plotRef(self, x0, y0, x1, y1):
-#        plt.plot([x0,x1],[y0,y1],linewidth=1)
-#        if x0 == x1:
-#            plt.ylim([y0,y1])
-#        else:
-#            plt.xlim([x0,x1])
-#
-#
-#    def plotTrace(self, time, voltage, label=""):
-#        assert self.trace == True
-#        self.times.append(time)
-#        self.traces.append(voltage)
-#        self.labels.append(label)
-#
-#    def save(self):
-#        #plt.subplots(len(self.traces),1)
-#        plt.rcParams.update({'font.size': 4})
-#        i = 1
-#        for label,time,trace in zip(self.labels,self.times,self.traces):
-#            plt.subplot(len(self.traces),1,i)
-#            plt.plot(time,trace)
-#            plt.gca().get_xaxis().set_visible(False)
-#            plt.ylabel(f"{label} (V)")
-#            i += 1
-#        plt.gca().get_xaxis().set_visible(True)
-#        plt.savefig(f"graph_{self.name}.png", dpi=200)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -134,53 +125,6 @@ def main():
 
     u = Analysis(args.filename)
     u.plotAll()
-
-    #def print_guide(name, l):
-    #    print(f"{name}")
-    #    s = ""
-    #    for i in range(l):
-    #        s = f"{str(hex(i % 16))[2]}{s}"
-    #    print(s)
-
-    #for test in [v]:
-    #    print_guide("D", 70)
-    #    samples = test.getSampleTaps()
-    #    for s in samples:
-    #        print(f"{s}:\t{samples[s]}")
-
-    #print("Sampled Data:")
-    #for test in [v]:
-    #    print_guide("Q", 48)
-    #    samples = test.getSamples()
-    #    for s in samples:
-    #        print(f"{s}:\t{samples[s]}")
-
-
-    #p = Plotter("q_trace", trace=True)
-    #taps = v.getTrace()
-    #p.plotTrace(v.getTime(),v.getVdd(),"VDD")
-    #p.plotTrace(v.getTime(),v.getClock(), "CLK")
-    #for i in range(12,22):
-    #    p.plotTrace(v.getTime(),v.getData()[f"{i}"],f"Q[{i}]")
-    #p.save()
-
-    #p = Plotter("d_trace", trace=True)
-    #taps = v.getTrace()
-    #p.plotTrace(v.getTime(),v.getVdd(),"VDD")
-    #p.plotTrace(v.getTime(),v.getClock(), "CLK")
-    #for i in range(25,35):
-    #    p.plotTrace(v.getTime(),v.getTaps()[f"{i}"],f"Q[{i}]")
-    #p.save()
-
-    ## Plot the delays
-    #p = Plotter("taps", taps=True)
-    #p.plotTaps(v.getTapCrossings(), ["750mV","770mV","790mV","810mV","830mV", "850mV"])
-    #p.plotRef(0,2e-8,50,2e-8)
-    #p.plotRef(21,0,21,3e-8)
-    #p.plotRef(38,0,38,3e-8)
-    #p.plotRef(39,0,39,3e-8)
-    #p.plotRef(40,0,40,3e-8)
-    #p.save()
 
 if "__main__" == __name__:
     main()
