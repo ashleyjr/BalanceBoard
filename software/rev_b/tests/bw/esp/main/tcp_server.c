09a8fae036e7442a196a560a5e36ef60ef9b6f7e
Ashley J. Robinson <ashley181291@gmail.com>
2024-03-20 23:14:42 +0000

    bw test

--- software/rev_b/tests/bw/esp/main/tcp_server.c
+++ software/rev_b/tests/bw/esp/main/tcp_server.c
@@ -29,37 +29,30 @@
 #define KEEPALIVE_INTERVAL          CONFIG_EXAMPLE_KEEPALIVE_INTERVAL
 #define KEEPALIVE_COUNT             CONFIG_EXAMPLE_KEEPALIVE_COUNT
 
+#define MAX_SIZE 1024
 static const char *TAG = "example";
 
-static void do_retransmit(const int sock)
-{
-    int len;
-    char rx_buffer[128];
-
-    do {
-        len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-        if (len < 0) {
-            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
-        } else if (len == 0) {
-            ESP_LOGW(TAG, "Connection closed");
-        } else {
-            rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
-            ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
-
-            // send() can return less bytes than supplied length.
-            // Walk-around for robust implementation.
-            int to_write = len;
-            while (to_write > 0) {
-                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-                if (written < 0) {
-                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                    // Failed to retransmit, giving up
-                    return;
-                }
-                to_write -= written;
-            }
-        }
-    } while (len > 0);
+static void do_retransmit(const int sock){
+   int i;
+   int len;
+   char buf[MAX_SIZE]; 
+   // Handle socket 
+   do {
+      len = recv(sock, buf, sizeof(buf) - 1, 0);
+      if (len < 0) {
+         ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+      } else if (len == 0) {
+         ESP_LOGW(TAG, "Connection closed");
+      } else {
+         buf[len] = 0; // Null-terminate whatever is received and treat it like a string
+         len = atoi(buf);
+         ESP_LOGI(TAG, "Generate %d byte(s) to send to client", len);
+         for(i=0;i<len;i++){
+            buf[i] = len >> 4;
+         }     
+         send(sock, buf, len, 0);
+      }
+   } while (len > 0);
 }
 
 static void tcp_server_task(void *pvParameters)
--- software/rev_b/tests/bw/python/test.py
+++ software/rev_b/tests/bw/python/test.py
@@ -1,15 +1,55 @@
 # echo-client.py
 
 import socket
+import time
 
 HOST = "192.168.1.153"  # The server's hostname or IP address
 PORT = 3333  # The port used by the server
 
+PKTS = 1024
+SIZE = 1024
+
+print(f"Sending {PKTS} packets of {SIZE} bytes")
+
+# Latency
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-
-    for i in range(1024):
-        s.sendall(b"1024")
+    latency_ms = []
+    for i in range(PKTS):
+        start = time.time()
+        s.sendall(bytes(f"1", encoding="ascii"))
         data = s.recv(1024)
+        end = time.time()
+        latency_ms.append((end - start)*1000)
 
-print(f"Received {data!r}")
+mini = min(latency_ms)
+maxi = max(latency_ms)
+mean = sum(latency_ms) / PKTS
+print(f"Latency:")
+print(f"\tMin  = {mini:.2f}ms")
+print(f"\tMax  = {maxi:.2f}ms")
+print(f"\tMean = {mean:.2f}ms")
+
+# Bandwidth
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    start = time.time()
+    for i in range(PKTS):
+        s.sendall(bytes(f"{SIZE}", encoding="ascii"))
+        data = s.recv(1024)
+    end = time.time()
+
+print(f"Bandwidth:")
+bytes_sent = PKTS*SIZE
+elapsed = end - start
+bytes_per_second = bytes_sent / elapsed
+kb_per_second = bytes_per_second / 1024
+mb_per_second = kb_per_second / 1024
+print(f"\tRaw              = {mb_per_second:.2f}MB/s")
+bytes_sent = PKTS*SIZE
+elapsed = (end - start) - (PKTS * (mean/1000))
+bytes_per_second = bytes_sent / elapsed
+kb_per_second = bytes_per_second / 1024
+mb_per_second = kb_per_second / 1024
+print(f"\tSubtract latency = {mb_per_second:.2f}MB/s")
+
